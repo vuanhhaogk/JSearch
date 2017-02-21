644112ce9300ec7b6dfce059cd2cdf469db62de4
Vu Anh Hao <Vu Anh Hao>
2017-02-21 14:22:54 +0700

    Complete jsearch v1.0.0 and Readme

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # JSearch
 Javascript Search Enviroment
+
+## Config
+
+```html
+<script src="path-to-library/jsearch.js">
+```
+
+## Basic
+
+**HTML**
+
+```html
+<input type="text" id="search">
+<div id="result"></div>
+```
+
+**JS**
+
+```js
+var env = new JSearch.Env(
+    document.getElementById('search'),
+    document.getElementById('result')
+);
+
+env.add({
+    name: "log",
+    handler: function(q, mode){
+        this.html(q);
+    },
+    mode: [2],
+    pos: 1
+});
+```
+
+[Demo](test/index.html)
+
+## API
+
+### JSearch.Env
+
+```js
+var env = new JSearch.Env(input, output);
+```
+
++ `input`: input data (text input tag).
++ `output`: result will appear here (using block tag such as div, article,...).
+
+### env.add(config)
+
+Add search engine.
+
+#### config
+
++ **config.name**: Name of engine.
++ **config.mode**: Mode using exec handler. Ex: `config.mode = [1, 2]`
+    + `[1]`: When keydown
+    + `[2]`: When enter
+    + `[1, 2]`: Both key down and enter
++ **config.handler**: Handler search query. Ex: `config.handler = function(q, m){ this.html('HTML') }`
+    + `q`: text in input tag.
+    + `m`: mode to active engine.
+    + `this`: bind result.
+    + **this.html(html)**: write html data to result.
+    + **this.attr(name, value)**: set attribute to tag which contain this engine
++ **config.pos**: Position of engin's result in env's result.
+
+### env.run(mode)
+
+Run all search engine.
+
+**mode**
+
++ `1`: Run engine which contain mode [1] and [1, 2]
++ `2`: Run engine which contain mode [2] and [1, 2]
+
+## License
+
+MIT
--- jsearch.js
+++ jsearch.js
@@ -4,44 +4,67 @@ JSearch.Env = function(inp, out){
     this.inp = inp;
     this.out = out;
 
+    this.inp.className = (this.inp.className.length > 0 ? this.inp.className + ' ' : '') + 'jser-input';
+    this.out.className = (this.out.className.length > 0 ? this.out.className + ' ' : '') + 'jser-output';
+
     this.inp.addEventListener('keydown', function(e){
-        if (e.keyCode === 13)
-            this.run(1);
+        if (e.keyCode !== 13)
+            return;
+
+        this.run(2);
     }.bind(this));
 
     this.inp.addEventListener('keyup', function(e){
-        if (e.keyCode === 13){
+        if (e.keyCode === 13)
             return;
-        }
 
-        this.run(0);
+        this.run(1);
     }.bind(this));
 
     this.engines = [];
-
-    return this;
-}
-
-JSearch.Env.prototype.run = function(mode){ // mode: [0: when keydown but not enter] [1: when enter]
-    mode = mode in [0, 1, 2] ? mode : 2;
-
-    for (var i = 0; i < this.engines.length; i++)
-        if (this.engines[i].mode.indexOf(mode) !== -1){
-            this.engines[i].handler.bind(this.engines[i])(this.inp.value, mode);
-        }
 }
 
 JSearch.Env.prototype.add = function(cfg){
+    cfg.pos = cfg.pos || this.engines.length + 1000;
+
     var elm = document.createElement('div');
-    this.out.appendChild(elm);
+
+    elm.className = 'jser-out-item';
+    elm.setAttribute('pos', cfg.pos);
+
+    var items = this.out.children;
+
+    var i = items.length - 1
+    while (i > 0 && parseInt(items[i].getAttribute('pos')) > cfg.pos)
+        i--;
+
+    this.out.insertBefore(elm, items[i]);
+
     this.engines.push({
-        name: cfg.name || "unknown",
+        name: cfg.name || 'unknown',
+        pos: cfg.pos,
         handler: cfg.handler || function(){},
-        pos: cfg.pos || this.engines.length + 1,
         mode: cfg.mode || [2],
         out: elm,
         html: function(html){
             this.out.innerHTML = html;
+        },
+        attr: function(name, value){
+            if (!value)
+                return this.out.getAttribute(name);
+
+            this.out.setAttribute(name, value);
+            return value;
         }
     });
+}
+
+JSearch.Env.prototype.run = function(mode){
+    mode = mode || 2;
+
+    for (var i = 0; i < this.engines.length; i++)
+        if (this.engines[i].mode.indexOf(mode) !== -1)
+            this.engines[i].handler.bind(this.engines[i])(this.inp.value, mode);
+        else
+            this.engines[i].html('');
 }
